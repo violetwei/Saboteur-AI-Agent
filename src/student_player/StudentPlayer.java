19077d1f515b67f5e1c0ca19181eb62cca0bd944
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2020-04-10 05:40:04 -0400

    Update StudentPlayer.java

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -4,6 +4,9 @@ import boardgame.Move;
 
 import Saboteur.SaboteurPlayer;
 import Saboteur.SaboteurBoardState;
+import Saboteur.cardClasses.*;
+import Saboteur.SaboteurMove;
+
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends SaboteurPlayer {
@@ -14,7 +17,7 @@ public class StudentPlayer extends SaboteurPlayer {
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("Version1");
     }
 
     /**
@@ -26,11 +29,37 @@ public class StudentPlayer extends SaboteurPlayer {
         // You probably will make separate functions in MyTools.
         // For example, maybe you'll need to load some pre-processed best opening
         // strategies...
-        MyTools.getSomething();
+        //MyTools.getSomething();
 
         // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
+        //Move myMove = boardState.getRandomMove();
 
+        // Return your move to be processed by the server.
+        //return myMove;
+
+        if(boardState.getTurnNumber() == 0) {
+    		MonteCarlo.readData();
+    	}
+    	if(boardState.getTurnNumber() <= 1) {
+    		Move myMove = boardState.getRandomMove();
+    		if (boardState.isLegal(new SaboteurMove(new SaboteurTile("8"), 7, 8, player_id))) {
+    			myMove = new SaboteurMove(new SaboteurTile("8"), 7, 8, 1);
+    		} else if(boardState.isLegal(new SaboteurMove(new SaboteurBonus(), 0, 0, player_id))) {
+    			myMove = new SaboteurMove(new SaboteurBonus(), 0, 0, 1);
+    		} else if(boardState.isLegal(new SaboteurMove(new SaboteurMap(), 12, 3, player_id))) {
+    			myMove = new SaboteurMove(new SaboteurMap(), 12, 3, playerId);
+    		} else if(boardState.isLegal(new SaboteurMove(new SaboteurDrop(), 4, 0, player_id))) {
+    			myMove = new SaboteurMove(new SaboteurDrop(), 4, 0, playerId);
+    		}
+    		return myMove;
+    	}
+    	Move myMove = null;
+    	try {
+        	myMove = MonteCarlo.random(boardState, player_id);
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
         // Return your move to be processed by the server.
         return myMove;
     }
